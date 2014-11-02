43ac4f2ec35806cc529ebcf0b72658652f873f4d
Bonspiel1 <cole_adama@shaw.ca>
2014-11-02 00:29:25 -0600

    Update Player.java
    CHNGS HVE BN MDE

--- code/Player.java
+++ code/Player.java
@@ -7,9 +7,9 @@ import java.awt.event.KeyEvent;
 import java.net.URL;
 
 public class Player {
-	private int x = 340, y = 540, dx = 0, width = 60, height = 40;
+	private int x = 340, y = 540, dx = 0, width = 60, height = 40, cooldwn = 0;
 	private boolean alive = true;
-	private int last = 2;
+	private int last = 2, lives =3;
 	Image Tank_Ref;
 	URL url;
 	public int getLast() {
@@ -61,6 +61,14 @@ public class Player {
 		this.height = height;
 	}
 
+	public int getCooldwn() {
+		return cooldwn;
+	}
+
+	public void setCooldwn(int cooldwn) {
+		this.cooldwn = cooldwn;
+	}
+
 	public boolean isAlive() {
 		return alive;
 	}
@@ -71,14 +79,16 @@ public class Player {
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
 	public void update(Main mc){
-		if(last==1)
-			Tank_Ref = Pictures.TankGreenLeft;
-		else if(last==2)
-			Tank_Ref = Pictures.TankGreenLeftStill;
-		else if(last==3)
-			Tank_Ref = Pictures.TankGreenRight;
-		else if(last==4)
-			Tank_Ref = Pictures.TankGreenRightStill;
+		if(alive){
+			if(last==1)
+				Tank_Ref = Pictures.TankGreenLeft;
+			else if(last==2)
+				Tank_Ref = Pictures.TankGreenLeftStill;
+			else if(last==3)
+				Tank_Ref = Pictures.TankGreenRight;
+			else if(last==4)
+				Tank_Ref = Pictures.TankGreenRightStill;
+		}
 		if(dx>0&&x+width<=800-dx&&alive)
 			x+=dx;
 		else if(dx<0&&x+2>=0+dx&&alive)
@@ -87,8 +97,6 @@ public class Player {
 
 	public void paint(Graphics g){
 		g.drawImage(Tank_Ref, x, y, Pictures.mc);
-		// g.fillRect(x,y,width,height);
-		//g.fill3DRect(x, y, width, height, true);
 	}
 
 	public void MoveLeft(){
@@ -103,5 +111,21 @@ public class Player {
 			dx = 5;
 			last = 3;
 		}	
+	}
+
+	public void Shoot(Main mc) {
+		for(int x=0; x<mc.bull.length; x++)
+			if(mc.bull[x].isAvail() && getCooldwn() <= 0 && alive){
+				setCooldwn(10);
+				if(last==1)
+					last=2;
+				else if(last==3)
+					last=4;
+				mc.bull[x].setX(last==1||last==2?this.x+27:this.x+9);
+				mc.bull[x].setY(this.y-height-10);
+				mc.bull[x].setDy(-4);
+				Sounds.TankShot.play();
+				break;
+			}
 	}
 }
